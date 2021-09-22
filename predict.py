d6e5cf4e30247e12d330537676f9ba63867cfaa0
ericguizzo <eric.guizzo@hotmail.it>
2021-09-22 09:55:11 +0200

    cog-ified

--- predict.py
+++ predict.py
@@ -3,6 +3,9 @@ import tempfile
 from pathlib import Path
 
 import cog
+import torch
+from torchvision.utils import save_image
+from stylegan2_pytorch import ModelLoader
 
 class Predictor(cog.Predictor):
     def setup(self):
